532138dd0d250376244ecce4ca5ffa51a966e504
Mikkel Malmberg <mikkelm@gmail.com>
2009-03-17 13:56:40 +0100

    A working spec sample

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,8 +1,11 @@
 require File.join(File.dirname(__FILE__), "spec_helper")
-require 'sinatra/test/rspec'
 
-describe "Application" do
-  it "should have specs" do
-    pending
+describe 'My Way' do
+  include Sinatra::Test
+
+  it "says something about Sinatra" do
+    get '/'
+    response.should be_ok
+    response.body.should =~ /Sinatra did it his way, you know\.\.\./
   end
 end
